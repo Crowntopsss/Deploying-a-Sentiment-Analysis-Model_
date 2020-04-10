8af3ab7292717f2a72053a9400e98ee7cf941360
EC2 Default User <ec2-user@ip-172-16-90-50.us-east-2.compute.internal>
2020-04-10 15:46:39 +0000

    lamda creation

--- serve/predict.py
+++ serve/predict.py
@@ -69,11 +69,9 @@ def predict_fn(input_data, model):
     #       You should produce two variables:
     #         data_X   - A sequence of length 500 which represents the converted review
     #         data_len - The length of the review
-    words_X = review_to_words(input_data)
-    data_X, data_len = convert_and_pad(model.word_dict, words_X)
-    
-    #data_X = None
-    #data_len = None
+
+    words = review_to_words(input_data)
+    data_X, data_len = convert_and_pad(model.word_dict, words)
 
     # Using data_X and data_len we construct an appropriate input tensor. Remember
     # that our model expects input data of the form 'len, review[500]'.
@@ -85,13 +83,12 @@ def predict_fn(input_data, model):
 
     # Make sure to put the model into evaluation mode
     model.eval()
-    with torch.no_grad():
-        output = model.forward(data)
-        
+
     # TODO: Compute the result of applying the model to the input data. The variable `result` should
     #       be a numpy array which contains a single integer which is either 1 or 0
+    with torch.no_grad():
+        output = model.forward(data)
+
     result = np.round(output.numpy())
-    
-   
 
     return result
